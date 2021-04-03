f06bcca3ed121f8441309013c9d387f6a93b52ff
Soobrakay <Soobrakay@users.noreply.github.com>
2021-04-03 13:50:45 -0400

    Move configuration to setup.cfg

--- setup.py
+++ setup.py
@@ -1,18 +1,4 @@
-from setuptools import find_packages, setup
+"""Required for ``pip install --editable .`` to work."""
+import setuptools
 
-setup(
-    name="downloader",
-    version="0.2.0",
-    description="Downloads pn or talkpython files",
-    long_description="Downloads pn or talkpython.",
-    url="https://github.com/Soobrakay",
-    author="soobrakay",
-    author_email="Soobrakay@users.noreply.github.com",
-    license="MIT",
-    packages=find_packages('src'),
-    package_dir={"": "src"},
-    install_requires=["beautifulsoup4", "click", "more-itertools", "requests"],
-    entry_points=dict(console_scripts=["downloader=downloader:cli"]),
-    include_package_data=True,
-    zip_safe=False,
-)
+setuptools.setup()
